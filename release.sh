95a552d2e3d09b82394055c4732832af7569fcc7
Stefan Gussner <gstefan1995@gmail.com>
2024-04-09 14:54:26 +0200

    allow release type to be specified as a flag

--- release.sh
+++ release.sh
@@ -12,9 +12,17 @@ npm run lint
 # Run tests
 npm test
 
-#get type of release
-echo "What type of release is this? (major, minor, patch, none)"
-read releaseType
+#get type of release (can be specified in the -r flag)
+while getopts r: flag
+do
+    case "${flag}" in
+        r) releaseType=${OPTARG};;
+    esac
+done
+if [ -z "$releaseType" ]; then
+    echo "What type of release is this? (major, minor, patch, none)"
+    read releaseType
+fi
 
 # Bump version if needed
 if [ "$releaseType" != "none" ]; then
